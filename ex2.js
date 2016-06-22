6768d575142a7843080b943b0d5c78081720ce72
Whelpley <whelpley@gmail.com>
2016-06-22 13:43:17 -0700

    Complete part 2

--- ex2.js
+++ ex2.js
@@ -3,8 +3,7 @@ import React from 'react'
 export const FilterableProductTable = React.createClass({
   render: function () {
     return (
-      <div className = "filterableProductTable">
-
+      <div>
       </div>
     );
   }
