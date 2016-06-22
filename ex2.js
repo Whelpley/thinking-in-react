3c873a63be185094a64145cf68de071c95b9f146
Whelpley <whelpley@gmail.com>
2016-06-22 13:29:15 -0700

    Initial commit

---
+++ ex2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const FilterableProductTable = React.createClass({
+  render: function () {
+    return (
+      <div className = "filterableProductTable">
+
+      </div>
+    );
+  }
+});
